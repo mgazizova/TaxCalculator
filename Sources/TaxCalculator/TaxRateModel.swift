54d8fe62102e418cba13b1f4f8f8ff4e17eb5fc1
Mariia Gazizova <mgazizova5@gmail.com>
2023-03-01 17:08:57 +0100

    default value

--- Sources/TaxCalculator/TaxRateModel.swift
+++ Sources/TaxCalculator/TaxRateModel.swift
@@ -16,7 +16,7 @@ public struct TaxRateModel: Identifiable, Equatable, Hashable, Comparable {
     public var minValue: Double
     public var rate: Double
     
-    public init(id: UUID?, minValue: Double, rate: Double) {
+    public init(id: UUID? = nil, minValue: Double, rate: Double) {
         self.id = id ?? UUID()
         self.minValue = minValue
         self.rate = rate
